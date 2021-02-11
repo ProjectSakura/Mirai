aac79ca6689453cf5c30b657661c7ff4aa70f86d
ArunTeltia <teltia.arun18@gmail.com>
2021-02-11 13:06:10 +0530

    refractor code

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const loadLatest = require("./routes/loadlatest");
 const loaderRoute = require("./routes/loader");
 const app = express();
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -39,13 +38,10 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
--- routes/viewmanga.js
+++ routes/viewmanga.js
@@ -5,8 +5,6 @@ const router = express.Router();
 
 router.get("/", async function (req, res, next) {
   const url = req.query.url;
-  // http://localhost:3000/viewmanga?url=https://mangakakalot.com/chapter/sd925573/chapter_1
-  // https://mangapark.net/manga/solo-leveling/i2628794/c133
   const image = await mangaParkObj.getImageList(url);
   const information = await mangaParkObj.getChapterInfo(url);
   res.render("viewmanga", {
